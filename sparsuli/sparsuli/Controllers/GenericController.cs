b3557981d0edddea3ed0df8d07ee9949b73c4eb0
salamaleiqum <rezo.getiashvili2016@gipa.ge>
2017-11-25 15:33:45 +0400

    proeqti
    davamate proeqti

---
+++ sparsuli/sparsuli/Controllers/GenericController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sparsuli.Controllers
+{
+    public class GenericController : Controller
+    {
+        protected string Language { get; set; }
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+
+            if (filterContext.RouteData.Values.ContainsKey("language"))
+                Language = filterContext.RouteData.Values["language"].ToString().ToLower();
+            else
+                Language = "ka";
+
+            ViewBag.language = Language;
+
+            Thread.CurrentThread.CurrentCulture = new CultureInfo(Language);
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(Language);
+        }
+    }
+}
---
+++ sparsuli/sparsuli/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="sparsuli.MvcApplication" Language="C#" %>
